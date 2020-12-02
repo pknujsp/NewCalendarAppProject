07dfba31ea0d695d02cb3de3c3d8bfb1eb455f0c
othean <naversovc@gmail.com>
2020-12-03 00:11:52 +0900

    commit new build

--- app/src/main/java/com/zerodsoft/scheduleweather/ImprovedScrollingRecyclerView.java
+++ app/src/main/java/com/zerodsoft/scheduleweather/ImprovedScrollingRecyclerView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +14,10 @@ public class ImprovedScrollingRecyclerView extends RecyclerView
 {
     private float lastX = 0f;
     private float lastY = 0f;
-    private boolean scrolling = false;
     private LayoutManager layoutManager;
     private boolean allowScroll;
+    private boolean isScrolling;
+    public static final String TAG = "TEST_RECYCLER_VIEW";
 
     public ImprovedScrollingRecyclerView(@NonNull Context context)
     {
@@ -31,35 +33,58 @@ public class ImprovedScrollingRecyclerView extends RecyclerView
                                          int defStyleAttr)
     {
         super(context, attrs, defStyleAttr);
+        addOnScrollListener(new OnScrollListener()
+        {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState)
+            {
+                super.onScrollStateChanged(recyclerView, newState);
+                isScrolling = newState != SCROLL_STATE_IDLE;
+                Log.e(TAG, "onScrollStateChanged");
+            }
+
+        });
+        setOnTouchListener(new OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent)
+            {
+                view.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent e)
     {
-        layoutManager = getLayoutManager();
-        if (layoutManager == null)
-        {
-            return super.onInterceptTouchEvent(e);
-        }
-
+        /*
         switch (e.getActionMasked())
         {
             case MotionEvent.ACTION_DOWN:
+            {
                 lastX = e.getX();
                 lastY = e.getY();
-                Log.e("IMRPOVED_RECYCLER_VIEW", "ACTION DOWN");
-                allowScroll = false;
-                break;
-
+            }
+            break;
+            case MotionEvent.ACTION_UP:
+            {
+                float currentX = e.getX();
+                float currentY = e.getY();
+                allowScroll = (!(Math.abs(currentX - lastX) <= 30)) || (!(Math.abs(currentY - lastY) <= 30));
+            }
+            break;
             case MotionEvent.ACTION_MOVE:
-                float dx = Math.abs(e.getX() - lastX);
-                float dy = Math.abs(e.getY() - lastY);
-                allowScroll = dy > dx ? layoutManager.canScrollVertically() : layoutManager.canScrollHorizontally();
-                Log.e("IMRPOVED_RECYCLER_VIEW", "ACTION_MOVE");
-                break;
-        }
+            {
 
+            }
+            break;
+        }
         return allowScroll;
+
+         */
+
+        return super.onInterceptTouchEvent(e);
     }
 
 }
