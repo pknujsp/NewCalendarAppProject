9e74fe0f4d7050407f5ddc87836a10cabbceefaf
pknujsp desktop <naversovc@gmail.com>
2021-02-18 02:06:36 +0900

    test

--- app/src/main/java/com/zerodsoft/scheduleweather/event/EventActivity.java
+++ app/src/main/java/com/zerodsoft/scheduleweather/event/EventActivity.java
@@ -265,7 +265,7 @@ public class EventActivity extends AppCompatActivity implements ILocation, IFab
     {
         // 참석자 - 알림 - 이벤트 순으로 삭제 (외래키 때문)
         // db column error
-        // fix error
+        // fix error (0206)
         calendarViewModel.deleteAllAttendees(calendarId, eventId);
         calendarViewModel.deleteAllReminders(calendarId, eventId);
         calendarViewModel.deleteEvent(calendarId, eventId);
